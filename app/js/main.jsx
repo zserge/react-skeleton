833d67eba92cfe01f383ab45cb7b33a3deca90af
Serge A. Zaitsev <zaitsev.serge@gmail.com>
2015-04-05 17:49:28 +0300

    added flux, changed main class to a more functional example

--- app/js/main.jsx
+++ app/js/main.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import AppDispatcher from './dispatcher';
 
 export default class Main extends React.Component {
+
+	constructor() {
+		super();
+
+		// ES6 binding workaround
+		this.onButtonClicked = this.onButtonClicked.bind(this);
+
+		// State
+		this.state = {
+			greeting: 'Hello, world',
+		};
+	}
+
+	onButtonClicked() {
+		this.setState({greeting: 'Goodbye, world!'});
+	}
+
   render() {
-    return (
-      <h1>Hello, world.</h1>
+		return (
+			<div>
+				<h1>{this.state.greeting}</h1>
+				<button onClick={this.onButtonClicked}>Good bye!</button>
+			</div>
     );
   }
 }
